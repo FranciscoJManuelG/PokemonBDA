5809e2f12b1da8cf02457ac817e268d5c3008e3c
Francisco Javier Manuel García <francisco.j.manuelg@udc.es>
2019-05-09 13:38:22 +0200

    ADD eliminar atque y pokemon

--- login.sql
+++ login.sql
@@ -1,7 +1,16 @@
 def_editor=pico
 set pages 50
 set feed 1
+column idp format 90
+column ida format 90
+column idh format 90
+column idt format 90
+column hp format 9990
+column atk format 9990
+column def format 9990
+column spe format 9990
 
 set linesize 100
 
 column descripcion format a50
+column nombrep format a20
